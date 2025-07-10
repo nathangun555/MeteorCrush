c72acf1ec8110af81059264083801a91245aff2b
nathangunawan <nathangun23@gmail.com>
2025-07-10 14:33:07 +0700

    Initial Commit

---
+++ MeteorCrush/MeteorCrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteorCrushApp.swift
+//  MeteorCrush
+//
+//  Created by Nathan Gunawan on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MeteorCrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeteorCrushTests/MeteorCrushTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeteorCrushTests.swift
+//  MeteorCrushTests
+//
+//  Created by Nathan Gunawan on 10/07/25.
+//
+
+import Testing
+@testable import MeteorCrush
+
+struct MeteorCrushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
